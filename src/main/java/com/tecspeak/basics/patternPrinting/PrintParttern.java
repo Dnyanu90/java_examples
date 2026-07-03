4b793f103813d942e5234e5c63decb2a6555e749
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-03 07:27:00 +0530

    Upadate Code

--- src/main/java/com/tecspeak/basics/patternPrinting/PrintParttern.java
+++ src/main/java/com/tecspeak/basics/patternPrinting/PrintParttern.java
@@ -1,62 +1,76 @@
 package com.tecspeak.basics.patternPrinting;
 
 public class PrintParttern {
-    static void Square(int n){
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
+    static void Square(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-    static void RightTriangle(int n){
-        for (int i=n;i>0;i--){
-            for (int j=i-1;j<n;j++){
+    static void RightTriangle(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = i - 1; j < n; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-    static void InvertedRightTriangle(int n){
-        for (int i=0;i<n;i++){
-            for (int j=i;j<n;j++){
+    static void InvertedRightTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
                 System.out.printf("*");
             }
             System.out.println();
         }
     }
 
-    static void Pyramid(int n){
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n-i-1;j++) {
+    static void Pyramid(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
-                for (int k=0;k<2*i+1;k++){
-                    System.out.print("*");
-                }
-                System.out.println();
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
             }
-
+            System.out.println();
+        }
     }
 
-    static  void HolllowRectangle(int n){
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=n;j++){
-                if (i==1 || j==1 || i==n || j==n){
+    static void HolllowRectangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n) {
                     System.out.print("*");
-                }else {
+                } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
         }
     }
-    static void NumTriangle(int n){
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=i;j++){
+
+    static void NumTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void XPrinting(int n) {
+        for (int i=1;i<n;i++){
+            for (int j=1;j<n;j++){
+                if (j==i || j==n-i){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
@@ -69,6 +83,8 @@ public class PrintParttern {
 //        InvertedRightTriangle(4);
 //        Pyramid(7);
 //        HolllowRectangle(4);
-        NumTriangle(5);
+//         NumTriangle(5);
+        XPrinting(6);
+
     }
 }
