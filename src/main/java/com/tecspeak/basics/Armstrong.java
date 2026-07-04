0b17f0a76bf2f5948751892728a26cdd1004d11b
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-04 15:31:43 +0530

    First Commit

---
+++ src/main/java/com/tecspeak/basics/Armstrong.java
@@ -0,0 +1,38 @@
+package com.tecspeak.basics;
+
+public class Armstrong {
+    static boolean checkArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        int digits = 0;
+        int temp = num;
+
+        while (temp > 0) {
+            digits++;
+            temp = temp / 10;
+        }
+
+        temp = num;
+
+
+        while (temp > 0) {
+
+            int digit = temp % 10;
+            int power = 1;
+
+            for (int i = 1; i <= digits; i++) {
+                power = power * digit;
+            }
+
+            sum = sum + power;
+
+            temp = temp / 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkArmstrong(153));
+    }
+}
