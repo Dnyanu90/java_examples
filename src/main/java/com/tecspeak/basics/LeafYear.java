a14c8bc3653535b2bd231ce5f10f7010f1cf5948
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-05 08:35:38 +0530

    Using Recursion

--- src/main/java/com/tecspeak/basics/LeafYear.java
+++ src/main/java/com/tecspeak/basics/LeafYear.java
@@ -10,8 +10,28 @@ public class LeafYear {
             System.out.println(check);
         }
     }
+    static boolean isLeap(int year, int step) {
+
+        if (step == 0) {
+            if (year % 400 == 0) return true;
+            return isLeap(year, 1);
+        }
+
+        if (step == 1) {
+            if (year % 100 == 0) return false;
+            return isLeap(year, 2);
+        }
+
+        if (step == 2) {
+            if (year % 4 == 0) return true;
+            return false;
+        }
+
+        return false;
+    }
 
     public static void main(String[] args) {
-        leafYear(2000);
+//        leafYear(2000);
+        System.out.println(isLeap(1900,0));
     }
 }
