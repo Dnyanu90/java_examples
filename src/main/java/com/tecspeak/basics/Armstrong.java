164be12f535213f56dbd732cf1c1da9d9904ee02
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-05 08:34:57 +0530

    Using Recursion

--- src/main/java/com/tecspeak/basics/Armstrong.java
+++ src/main/java/com/tecspeak/basics/Armstrong.java
@@ -32,7 +32,16 @@ public class Armstrong {
         return sum == original;
     }
 
+    static int arm(int n) {
+        if (n == 0)
+            return 0;
+
+        int d = n % 10;
+        return d*d*d + arm(n / 10);
+    }
+
     public static void main(String[] args) {
         System.out.println(checkArmstrong(153));
+        arm(5);
     }
 }
