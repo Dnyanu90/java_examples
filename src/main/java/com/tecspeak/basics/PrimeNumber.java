bc88ea7aaec7dcbfa8c814f438309dc55b8e1d9d
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-05 08:36:14 +0530

    Using Recursion

--- src/main/java/com/tecspeak/basics/PrimeNumber.java
+++ src/main/java/com/tecspeak/basics/PrimeNumber.java
@@ -21,7 +21,21 @@ public class PrimeNumber {
         }
     }
 
+    static boolean isprime(int n,int i) {
+        if (n>=2){
+            return (n==2);
+        }
+        if (n%i==0){
+            return false;
+        }
+        if (i*i>n){
+            return true;
+        }
+        return isprime(n,i+1);
+    }
+
     public static void main(String[] args) {
         primeNumber(100);
+        isprime(10,5);
     }
 }
