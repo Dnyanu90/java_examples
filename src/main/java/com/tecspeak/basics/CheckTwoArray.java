a5da733f161ef67bb9ce394e668604a21f57927f
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-05 13:02:23 +0530

    Update Condition

--- src/main/java/com/tecspeak/basics/CheckTwoArray.java
+++ src/main/java/com/tecspeak/basics/CheckTwoArray.java
@@ -3,6 +3,9 @@ package com.tecspeak.basics;
 public class CheckTwoArray {
     public static boolean checkTwoArray(int arrOne[], int arrTwo[]) {
         boolean check = true;
+        if (arrTwo.length!=arrOne.length){
+            check =false;
+        }
         for (int i = 0; i < arrOne.length; i++) {
             if (arrTwo[i] != arrOne[i]) {
                 check = false;
@@ -13,7 +16,7 @@ public class CheckTwoArray {
 
     public static void main(String[] args) {
         int arrOne[] = {1, 2, 3, 4};
-        int arrTwo[] = {1, 2, 3, 7};
+        int arrTwo[] = {1, 2, 3, 4};
         System.out.println(checkTwoArray(arrOne, arrTwo));
     }
 
