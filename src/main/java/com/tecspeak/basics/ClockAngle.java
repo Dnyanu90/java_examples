bd1a3b5d8cd3073e64a625867137ea119ebd4102
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-11 15:29:42 +0530

    Find Clock Angle

---
+++ src/main/java/com/tecspeak/basics/ClockAngle.java
@@ -0,0 +1,20 @@
+package com.tecspeak.basics;
+
+public class ClockAngle {
+    public static void getAngle(int hour, int minute) {
+        hour = hour % 12;
+        double hourAngle = hour * 30 + minute * 0.5;
+        double minuteAngle = minute * 6;
+
+        double angle = Math.abs(hourAngle - minuteAngle);
+        System.out.println(Math.min(angle, 360 - angle));
+        System.out.println(Math.max(angle, 360 - angle));
+
+    }
+
+    public static void main(String[] args) {
+        getAngle(23,59);
+
+    }
+
+}
