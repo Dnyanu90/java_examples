dd7f43ac4bf22b866cda9bde14151da8355a8f4e
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-21 17:26:10 +0530

    Update Conditin

--- src/main/java/com/tecspeak/basics/patternPrinting/ChessBoardPrintting.java
+++ src/main/java/com/tecspeak/basics/patternPrinting/ChessBoardPrintting.java
@@ -1,14 +1,20 @@
 package com.tecspeak.basics.patternPrinting;
 
 public class ChessBoardPrintting {
+
+
     public static void main(String[] args) {
         int n=8;
         for (int i=0;i<n;i++){
+
             for (int j=0;j<n;j++){
+                if (i==1 || i==6){
+                    System.out.print("P");
+                }
                 if ((i+j)%2==0){
-                    System.out.print(" White ");
+                    System.out.print(" O ");
                 }else {
-                    System.out.print(" Black ");
+                    System.out.print(" X ");
                 }
             }
             System.out.println(" ");
