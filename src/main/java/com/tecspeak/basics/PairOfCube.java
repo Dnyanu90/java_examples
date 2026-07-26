cd297d04374001b012d41c99b1374243df1ea0ee
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-26 11:23:37 +0530

    Print Cube in I and J loop

--- src/main/java/com/tecspeak/basics/PairOfCube.java
+++ src/main/java/com/tecspeak/basics/PairOfCube.java
@@ -12,10 +12,24 @@ public class PairOfCube {
         }
         return count;
     }
+    public static int pairOFCuteInIAndJ(int n){
+        int count = 0;
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=n;j++){
+                if (i*i*i+j*j*j==n){
+                    System.out.println("Match found! i = " + i  + ", j = " + j  +" = "+ n);
+                    count ++;
+                }
+            }
+        }
 
-    
+        return  count;
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println(pairOfCubeCount(9));
+        System.out.println(pairOFCuteInIAndJ(28));
     }
 }
