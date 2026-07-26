eb468720928dc6e19fdf07ac5e76b097c1809d37
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-26 10:43:35 +0530

    Count Pair of Cube

---
+++ src/main/java/com/tecspeak/basics/PairOfCube.java
@@ -0,0 +1,21 @@
+package com.tecspeak.basics;
+
+public class PairOfCube {
+    public static int pairOfCubeCount(int n){
+        int count=0;
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=n;j++){
+                if (i*i*i+j*j*j==n){
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+
+    
+
+    public static void main(String[] args) {
+        System.out.println(pairOfCubeCount(9));
+    }
+}
