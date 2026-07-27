0f0c95f8468cb5c312f5e56a67400e3fb31f6d22
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-07-27 15:39:25 +0530

    Check String is Palindrome

---
+++ src/main/java/com/tecspeak/basics/strings/CheckPalindrome.java
@@ -0,0 +1,19 @@
+package com.tecspeak.basics.strings;
+
+import java.util.Scanner;
+
+public class CheckPalindrome {
+    public static boolean ispalindrome(String s){
+       for(int i=0;i<s.length()/2;i++){
+           if (s.charAt(i) !=s.charAt(s.length()-1-i)){
+               return false;
+           }
+       }
+       return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println( ispalindrome("ABDCBA"));
+    }
+}
