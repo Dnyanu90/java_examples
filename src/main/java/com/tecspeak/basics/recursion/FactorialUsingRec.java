594b249f768d34149e3bd9d8e1436d711d334c60
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-08-01 10:46:19 +0530

    Recursive Function For Factorial

---
+++ src/main/java/com/tecspeak/basics/recursion/FactorialUsingRec.java
@@ -0,0 +1,13 @@
+package com.tecspeak.basics.recursion;
+
+public class FactorialUsingRec {
+    public static int factorial(int n) {
+        if (n != 0)
+            return n * factorial(n - 1);
+        else
+            return 1;
+    }
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
