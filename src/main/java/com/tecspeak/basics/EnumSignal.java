f3824c7359087d30238675d019547a45a90bcddd
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-08-05 07:16:43 +0530

    Implented abstract method behave in enum

--- src/main/java/com/tecspeak/basics/EnumSignal.java
+++ src/main/java/com/tecspeak/basics/EnumSignal.java
@@ -1,11 +1,29 @@
 package com.tecspeak.basics;
 
 enum Signal {
-    RED("Stop", 30),
-    BLUE("Go", 40),
-    YELLOW("Wait", 5);
+    RED("Stop", 30){
+        void behave(){
+            System.out.println("Stop Immediately");
+        }
+    },
+    BLUE("Go", 40){
+        void behave(){
+            System.out.println("Go Fast");
+        }
+    },
+    YELLOW("Wait", 5){
+        void behave(){
+            System.out.println("Slow Down");
+        }
+
+    };
+    abstract  void behave();
     private String action;
     private int time;
+
+    void show(){
+        System.out.println(action +" For "+time+" seconds");
+    }
 
     public String getAction() {
         return action;
@@ -24,6 +42,8 @@ enum Signal {
 public class EnumSignal {
     public static void main(String[] args) {
         Signal s= Signal.RED;
+        s.show();
+        s.behave();
         System.out.println(s.getAction());
         System.out.println(s.getTime());
         System.out.println();
