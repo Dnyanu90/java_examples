aa4bd182d2c54e7af615c83f9839292488d9a14b
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-08-17 12:45:54 +0530

    Update code use contains method

--- src/main/java/com/tecspeak/collection/StudentCollectionExample.java
+++ src/main/java/com/tecspeak/collection/StudentCollectionExample.java
@@ -2,14 +2,18 @@ package com.tecspeak.collection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class StudentCollectionExample {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
         students.add(new Student(12, "Rahul"));
         students.add(new Student(13, "Vijay"));
-        students.add(new Student(12, "Kumar"));
+        students.add(new Student(15, "Kumar"));
+//        students.set(2, (Student) students);
         students.remove(0);  // delteting the 0th index of the Arraylist
+
+        System.out.println(students.size());
 
 
         for (Student student : students) {
@@ -25,6 +29,7 @@ public class StudentCollectionExample {
             }
         }
 
+        System.out.println(students.contains(new Student(13, "")) ? "present" : "absent");
 
     }
 }
@@ -52,5 +57,16 @@ class Student {
 
     public void setRoll(int roll) {
         this.roll = roll;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Student student)) return false;
+        return roll == student.roll;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(roll);
     }
 }
