fd6941f2cf1a96f592ee85a2b62bc5728a60ebb6
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-08-18 15:49:49 +0530

    Simple Exaqmple of LinkedList

---
+++ src/main/java/com/tecspeak/collection/LinkedListExample.java
@@ -0,0 +1,15 @@
+package com.tecspeak.collection;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<String> student=new LinkedList<>();
+
+        student.add("Vijay");
+        student.add("Rahul");
+        student.add("Pratik");
+
+        System.out.println(student);
+    }
+}
