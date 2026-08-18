82df3efe8b3448ca9f1fdbeeaf3f2c5815712c1f
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-08-18 15:54:38 +0530

    Using removeFirst() & removeLast() in LinkedList

--- src/main/java/com/tecspeak/collection/LinkedListExample.java
+++ src/main/java/com/tecspeak/collection/LinkedListExample.java
@@ -4,12 +4,24 @@ import java.util.LinkedList;
 
 public class LinkedListExample {
     public static void main(String[] args) {
-        LinkedList<String> student=new LinkedList<>();
+        LinkedList<String> student = new LinkedList<>();
 
         student.add("Vijay");
         student.add("Rahul");
         student.add("Pratik");
 
+        // Add at First
+        student.addFirst("Vikas");
+
+        System.out.println(student);
+
+        // Add Last
+        student.addLast("Kumar");
+        student.removeFirst();
+
+
+        // Remove First
+        student.removeLast();
         System.out.println(student);
     }
 }
