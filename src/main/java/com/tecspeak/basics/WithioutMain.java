6dbb5a56a27a0ac3c9a6eebcdbb35d5d140c9815
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-09-05 12:26:14 +0530

    Without Main Method()

---
+++ src/main/java/com/tecspeak/basics/WithioutMain.java
@@ -0,0 +1,12 @@
+package com.tecspeak.basics;
+
+public class WithioutMain {
+    static {
+        System.out.println("Hello Java");
+        System.exit(0);
+    }
+
+//    public static void main(String[] args) {
+
+//    }
+}
